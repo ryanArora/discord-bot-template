d2e1e580a6b8a2bb04a47a800f39be1968a94bd2
Ryan Arora <arora.ryan@gmail.com>
2022-02-10 05:15:22 -0500

    feat: actual discord bot, fixed swc

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@ import invariant from "tiny-invariant";
 dotenv.config();
 invariant(process.env.DISCORD_TOKEN, "DISCORD_TOKEN is required");
 
-console.log("Testing!", process.env["NODE_ENV"]);
+import path from "path";
+import Discord from "discord.js";
+import transformClient from "./client/transform";
+import { registerHooks } from "./client/hooks";
+import { registerCommands } from "./client/commands";
 
-let ctr = 0;
-setInterval(() => {
-  ctr++;
-  console.log("Counter: ", ctr);
-}, 1000);
+const client = transformClient(new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] }));
+registerHooks(client, path.join(__dirname, "hooks"));
+registerCommands(client, path.join(__dirname, "commands"));
+
+client.login(process.env.DISCORD_TOKEN);
