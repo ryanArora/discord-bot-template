291409749049fe43f31070306cb1c743cf087684
Ryan Arora <arora.ryan@gmail.com>
2022-02-10 04:25:25 -0500

    fix: add env to deploy, pm2

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,10 @@ import invariant from "tiny-invariant";
 dotenv.config();
 invariant(process.env.DISCORD_TOKEN, "DISCORD_TOKEN is required");
 
-console.log("Testing!!!");
+console.log("Testing!", process.env["NODE_ENV"]);
+
+let ctr = 0;
+setInterval(() => {
+  ctr++;
+  console.log("Counter: ", ctr);
+}, 1000);
